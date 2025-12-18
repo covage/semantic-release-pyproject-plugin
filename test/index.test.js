ae6d26038f056fe097911c5e2acde27b04a71b9e
Matthieu Bessat <matthieu.bessat@covage.com>
2025-12-18 17:52:34 +0100

    test: remove TOML syntax test no longer relevant

--- test/index.test.js
+++ test/index.test.js
@@ -78,24 +78,4 @@ describe('semantic release integration', () => {
         expect(writtenContent).toContain('version = "1.0.0"');
     });
 
-    test('replace invalid TOML', async () => {
-        // Mock context
-        const context = {
-            nextRelease: { version: '1.0.0' },
-            logger: { log: jest.fn() }
-        };
-        // Mock fs
-        const mockPyproject = {
-            tool: {
-                poetry: {
-                    version: '0.1.0'
-                }
-            }
-        };
-        fs.readFile = jest.fn().mockResolvedValue(toml.stringify(mockPyproject).replace("=", "}"));
-        fs.writeFile = jest.fn().mockResolvedValue();
-        await expect(async () => {
-            await plugin.prepare({}, context);
-        }).rejects.toThrow("Error while replacing version in pyproject.toml: Invalid TOML document");
-    });
 }); 
